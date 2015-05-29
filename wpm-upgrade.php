ba9a1c696536e44a829dab5efd0bd9717cbf3398
webfwd <webfwd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 18:00:18 +0000

    = 0.2 - Fix SQL error for counting subscribers. - Wording changes. - Decode html entities on subject. - Added Emogrifier by John Reeve to convert seperated HTML and CSS into inline HTML+CSS. - Changed bitly donate link as they think its spam. - Working pretty well with WP_DEBUG enabled. - Added WordPress version to credits form.
    git-svn-id: https://plugins.svn.wordpress.org/wp-mailer/tags/0.2@1170385 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpm-upgrade.php
@@ -0,0 +1,15 @@
+<?php
+defined('ABSPATH') or die('You can not access this file directly.');
+
+$plver = wpm_get_version();
+$dbver = get_option('wpm_version');
+
+if ($dbver > $plver){
+    deactivate_plugins(basename(__FILE__));
+    wp_die("Sorry, The plugin version you are installing is older than the database you have installed, please install plugin version ".$dbver." or newer.");
+}
+
+if ($dbver <= '0.1') {} // 29-May-2015 - Nothing to do in this version
+if ($dbver <= '0.2') {} // 29-May-2015 - Nothing to do in this version
+
+update_option('wpm_version', $plver);
